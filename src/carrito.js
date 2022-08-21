b93ab6156afbb0caebf31b7b746d23b1beb07c86
guillermoecr <62630841+guillermoecr@users.noreply.github.com>
2022-08-21 20:58:07 -0300

    Archivos de Carrito/Producto/Index (#2)
    * initial commit
    
    * carrito_remasterizado
    Se redujo la funcion de agregarProducto...
    
    * chore: eliminando texto con el requerimiento
    
    Co-authored-by: Mauro Gamboa <52142332+maurogamboa@users.noreply.github.com>

--- src/carrito.js
+++ src/carrito.js
@@ -1,4 +1,19 @@
+import { obtenerNombreProducto } from "./productos";
+
 /**
  * Agrega un producto al carrito de compras
  */
-export function agregarProducto(nombredelProducto) {}
+
+export function agregarProducto(nombredelProducto) {
+  //let product = document.getElementById("producto-1");
+  //let hijo0 = product.children[0];
+  let textHijo = obtenerNombreProducto(nombredelProducto);
+
+  let nuevoSpan = document.createElement("p");
+  let textoNuevo = document.createTextNode(textHijo);
+  nuevoSpan.appendChild(textoNuevo);
+
+  let carritoCompras = document.getElementById("carrito-compras");
+  carritoCompras.appendChild(nuevoSpan);
+  return nuevoSpan;
+}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import { obtenerNombreProducto } from "./productos";
+//import { obtenerNombreProducto } from "./productos";
+import { obtenerBotonComprar } from "./productos";
+import { agregarProducto } from "./carrito";
 import "./styles.css";
 
-/* 
- Requerimiento:
- ==================
- Al hacer click en el boton 'comprar' en cualquiera de los productos 
- de la lista, el nombre del producto seleccionado se debe
- agregar en el area del 'Carrito de compras'.
+function clickBoton1() {
+  agregarProducto("producto-1");
+}
+function clickBoton2() {
+  agregarProducto("producto-2");
+}
+function clickBoton3() {
+  agregarProducto("producto-3");
+}
+function clickBoton4() {
+  agregarProducto("producto-4");
+}
 
- Ejemplo: 
- 1. Click en 'comprar' en el producto llamado 'Bebida Energetica'
- 2. 'Bebida Energetica' debe aparecer debajo del area 'Carrito de compras'
+const botonUno = obtenerBotonComprar("producto-1");
+botonUno.addEventListener("click", clickBoton1);
 
- Notas tecnicas:
- - Cada producto tiene un ID (usar el metodo `getElementById`)
- - Puedes utilizar el metodo `children` (array) para acceder a los hijos de un elemento
- - Se debe agregar un evento (event listener) del tipo 'click' 
-   en los botones de cada producto
- - Se debe crear un funcion para manejar los eventos
-*/
+const botonDos = obtenerBotonComprar("producto-2");
+botonDos.addEventListener("click", clickBoton2);
 
-//Ejemplo para obtener el nombre del primer producto
-const nombreProducto1 = obtenerNombreProducto("producto-1");
-console.log("nombre:", nombreProducto1);
+const botonTres = obtenerBotonComprar("producto-3");
+botonTres.addEventListener("click", clickBoton3);
+
+const botonCuatro = obtenerBotonComprar("producto-4");
+botonCuatro.addEventListener("click", clickBoton4);
