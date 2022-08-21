8802e82ab2b171b2337ddf32d3de545b8414c04c
Mauro Gamboa <52142332+maurogamboa@users.noreply.github.com>
2022-08-21 20:54:55 -0300

    chore: eliminando texto con el requerimiento

--- src/index.js
+++ src/index.js
@@ -3,29 +3,6 @@ import { obtenerBotonComprar } from "./productos";
 import { agregarProducto } from "./carrito";
 import "./styles.css";
 
-/* 
- Requerimiento:
- ==================
- Al hacer click en el boton 'comprar' en cualquiera de los productos 
- de la lista, el nombre del producto seleccionado se debe
- agregar en el area del 'Carrito de compras'.
-
- Ejemplo: 
- 1. Click en 'comprar' en el producto llamado 'Bebida Energetica'
- 2. 'Bebida Energetica' debe aparecer debajo del area 'Carrito de compras'
-
- Notas tecnicas:
- - Cada producto tiene un ID (usar el metodo `getElementById`)
- - Puedes utilizar el metodo `children` (array) para acceder a los hijos de un elemento
- - Se debe agregar un evento (event listener) del tipo 'click' 
-   en los botones de cada producto
- - Se debe crear un funcion para manejar los eventos
-*/
-
-//Ejemplo para obtener el nombre del primer producto
-//const nombreProducto1 = obtenerNombreProducto("producto-1");
-//console.log("nombre:", nombreProducto1);
-
 function clickBoton1() {
   agregarProducto("producto-1");
 }
